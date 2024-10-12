a5e96efa51d0d3dbe96ccbb075205126081b4e64
Rafly1818 <raflyravsanjani@gmail.com>
2024-10-12 23:19:38 +0700

    Add all contents of src folder

---
+++ src/Pert4/BilanganPrima.java
@@ -0,0 +1,26 @@
+package Pert4;
+
+public class BilanganPrima {
+    public static void main(String[] args) {
+        int batasBawah = 2, batasAtas = 30;
+
+        System.out.println("Bilangan prima antara " + batasBawah + " dan " + batasAtas + " adalah: ");
+
+        //looping untuk tiap bilangan
+        for (int i = batasBawah; i <= batasAtas; i++) {
+            boolean prima = true;
+
+            //looping untuk cek bilangan prima
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+
+            if (prima) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
