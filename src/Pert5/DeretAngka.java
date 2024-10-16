9b33b0e059fd8ee5cbd82ae2b726293e9e5af596
Rafly1818 <raflyravsanjani@gmail.com>
2024-10-16 15:23:27 +0700

    fresh

---
+++ src/Pert5/DeretAngka.java
@@ -0,0 +1,9 @@
+package Pert5;
+
+public class DeretAngka {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iterasi " + i + ": " + i);
+        }
+    }
+}
