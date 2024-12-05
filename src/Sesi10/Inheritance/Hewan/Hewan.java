bbe1ef2f9ede3eee08f222bf984c631ffa294521
Rafly1818 <raflyravsanjani@gmail.com>
2024-12-05 15:39:19 +0700

    fresh

--- src/Sesi10/Inheritance/Hewan/Hewan.java
+++ src/Sesi10/Inheritance/Hewan/Hewan.java
@@ -166,31 +166,3 @@ class Lele extends Ikan {
         System.out.println("Berat: " +Berat+ "Kg");
     }   
 }
-
-class Pewarisan {
-    public static void main(String[] args) {
-        Sapi sp1 = new Sapi();
-        sp1.cetak();
-        System.out.println("");
-
-        Kambing kmb1 = new Kambing();
-        kmb1.cetak();
-        System.out.println("");
-
-        Burung brg1 = new Burung();
-        brg1.cetak();
-        System.out.println("");
-
-        Ayam aym1 = new Ayam();
-        aym1.cetak();
-        System.out.println("");
-
-        Gurami grm1 = new Gurami();
-        grm1.cetak();
-        System.out.println("");
-
-        Lele ll1 = new Lele();
-        ll1.cetak();
-        System.out.println("");
-    }
-}
---
+++ src/Sesi10/Inheritance/Hewan/Main.java
@@ -0,0 +1,29 @@
+package Sesi10.Inheritance.Hewan;
+
+public class Main {
+        public static void main(String[] args) {
+            Sapi sp1 = new Sapi();
+            sp1.cetak();
+            System.out.println("");
+    
+            Kambing kmb1 = new Kambing();
+            kmb1.cetak();
+            System.out.println("");
+    
+            Burung brg1 = new Burung();
+            brg1.cetak();
+            System.out.println("");
+    
+            Ayam aym1 = new Ayam();
+            aym1.cetak();
+            System.out.println("");
+    
+            Gurami grm1 = new Gurami();
+            grm1.cetak();
+            System.out.println("");
+    
+            Lele ll1 = new Lele();
+            ll1.cetak();
+            System.out.println("");
+        }
+}
