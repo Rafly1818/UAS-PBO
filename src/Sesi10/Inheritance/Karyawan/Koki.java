d60b0010ca1448f833b1f1146b9a7dfbac5b10f5
Rafly1818 <raflyravsanjani@gmail.com>
2024-12-09 16:08:41 +0700

    fresh

---
+++ src/Sesi10/Inheritance/Karyawan/Koki.java
@@ -0,0 +1,23 @@
+package Sesi10.Inheritance.Karyawan;
+
+public class Koki extends Pegawai {
+    
+    // Tambahkan @Override diatas fungsi void menampilkan ().
+    // Override sendiri berfungsi sebagai pembuatan ulang method dari superclass untuk subclass.
+    @Override
+
+    public void menampilkan()
+    {
+        // Untuk nilai dari void menampilkan bisa berbeda dari nilai yang ada pada superclass.
+        System.out.println("Nama: " + nama);
+        System.out.println("Id_Pegawai: " + id_pegawai);
+        System.out.println("Gaji: " + gaji);
+    }
+
+    public void tugas()
+    {
+        // Fungsi dari void tugas() merupakan fungsi spesifik yang hanya dimiliki oleh class
+        System.out.println("Tugas: Memasak makanan dan membuat minuman");
+        System.out.println("========================================================");
+    }
+}
