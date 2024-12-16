1d3785fb6e0cc83e9f57d2a4208aea1a4a068a8b
Rafly1818 <raflyravsanjani@gmail.com>
2024-12-16 15:46:24 +0700

    fresh

---
+++ src/Sesi11/Polimorfisme/Tugas/OperasiBilangan1/Polimorfisme/Main.java
@@ -0,0 +1,22 @@
+package Sesi11.Polimorfisme.Tugas.OperasiBilangan1.Polimorfisme;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Nilai A dan B
+        double a = 10.5;
+        double b = 0.5;
+
+        // Array Operasi
+        OperasiBilangan[] operasiArray = {
+            new OperasiPenjumlahan(),
+            new OperasiPengurangan(),
+            new OperasiPerkalian(),
+            new OperasiPembagian()
+        };
+
+        // Cetak semua operasi
+        PrintOperasi cetak = new PrintOperasi();
+        cetak.cetakSemua(operasiArray, a, b);
+    }   
+}
