4efc78bea1aad67cb82bd3102074615fb87765d5
Ravsanjani <156632613+Rafly1818@users.noreply.github.com>
2025-02-05 20:26:45 +0700

    Delete src/Kuis4 directory

--- src/Kuis4/Soal4/Aves/Merpati.java
+++
@@ -1,7 +0,0 @@
-package Kuis4.Soal4.Aves;
-
-public final class Merpati extends Aves {
-    public Merpati(String nama, int ukuran) {
-        super(nama, ukuran);
-    }
-}
--- src/Kuis4/Soal4/Interfaces/AvesInterface.java
+++
@@ -1,8 +0,0 @@
-package Kuis4.Soal4.Interfaces;
-
-public interface AvesInterface {
-    void setJenisAves(String jenisAves);
-    String getJenisAves();
-    void setBisaTerbang(Boolean bisaTerbang);
-    Boolean getBisaTerbang();
-}
